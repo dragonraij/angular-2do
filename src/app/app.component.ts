5d1d50163a363821bac87ba9a7dc89d6b136d246
Sunila Prasad <rajeshwarrprasad@yahoo.com>
2017-09-05 23:50:43 +1200

    import forms and add dependency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+// Import class so we can register it as dependency injection token
+import {Todo} from './todo';
+import {TodoDataService} from './todo-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [TodoDataService]
 })
 export class AppComponent {
-  title = 'app running';
+
+    newTodo: Todo = new Todo();
+  
+  // Ask Angular DI system to inject the dependency
+  // associated with the dependency injection token `TodoDataService`
+  // and assign it to a property called `todoDataService`
+  constructor(private todoDataService: TodoDataService) {
+  }
+
+  // Service is now available as this.todoDataService
+  toggleTodoComplete(todo) {
+    this.todoDataService.toggleTodoComplete(todo);
+  
+  }
+  
+  addTodo() {
+    this.todoDataService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  removeTodo(todo) {
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    return this.todoDataService.getAllTodos();
+  }
+  
 }
